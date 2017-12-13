e4ab61063b58d3897ea0c7ff22843e2c0aaf8de3
songshu189 <songshu.yu@yahoo.com>
2017-12-13 16:52:01 -0500

    Test for second to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -21,7 +21,6 @@ class NewVisitorTest(unittest.TestCase):
         # to check out its homepage
 
         self.browser.get('http://localhost:8080')
-        time.sleep(2)
 
         # She notices the page title and header mention to-do lists
         assert 'To-Do' in self.browser.title, "Browser title was " + self.browser.title
@@ -47,13 +46,27 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            f"New to-do item did not appear in table. Contents were:\n{table.text}"
         )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+             [row.text for row in rows]
+        )
+
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
