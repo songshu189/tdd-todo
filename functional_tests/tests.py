5677537f5948eae31eab732fe10fb7cbe4098c64
songshu189 <songshu.yu@yahoo.com>
2017-12-14 17:35:11 -0500

    Wait until table element loaded

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,11 @@
 import time
 
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from django.test import LiveServerTestCase
 
@@ -19,9 +23,14 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+        try:
+            table = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.ID, "id_list_table"))
+            )
+            rows = table.find_elements_by_tag_name('tr')
+            self.assertIn(row_text, [row.text for row in rows])
+        except TimeoutException:
+            self.assertTrue(False)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -48,7 +57,7 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        #time.sleep(1)
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
@@ -57,7 +66,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        #time.sleep(1)
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
