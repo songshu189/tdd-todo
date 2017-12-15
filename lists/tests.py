7103a849923a10d3f500056b07fc99c3424adb99
songshu189 <songshu.yu@yahoo.com>
2017-12-15 16:03:46 -0500

    Separate home and list templates, add view, url, tests

--- lists/tests.py
+++ lists/tests.py
@@ -18,7 +18,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 
 class ItemModelTest(TestCase):
@@ -39,3 +39,19 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+class ListViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'item 1')
+        self.assertContains(response, 'item 2')
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
--- lists/views.py
+++ lists/views.py
@@ -6,7 +6,9 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return HttpResponseRedirect('/')
-    return render(request, 'home.html', {
-        'items': Item.objects.all(),
-    })
+        return HttpResponseRedirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
+
+
+def view_list(request):
+    return render(request, 'list.html', {'items': Item.objects.all(),})
