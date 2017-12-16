14bbbf0f4c64c0bbbbac6db52856e9a3b5d6c675
songshu189 <songshu.yu@yahoo.com>
2017-12-15 21:03:17 -0500

    Rearrange urls

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from lists import views as list_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('', include('lists.urls')),
+    url(r'^$', list_views.home_page, name='home'),
+    url('^lists/', include('lists.urls')),
 ]
