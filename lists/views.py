d8e10a7fb45613aeb1cb7d9f9e9c8db1f9740121
songshu189 <songshu.yu@yahoo.com>
2018-01-10 13:00:59 -0500

    Process empty list item input

--- lists/views.py
+++ lists/views.py
@@ -7,19 +7,31 @@ def home_page(request):
     return render(request, 'home.html')
 
 def new_list(request):
-    list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return HttpResponseRedirect(f'/lists/{list_.id}/')
+    text = request.POST['item_text']
+    text = text.strip()
+    if text:
+        list_ = List.objects.create()
+        Item.objects.create(text=text, list=list_)
+        return HttpResponseRedirect(f'/lists/{list_.id}/')
+    return render(request, 'home.html', {
+        'errors': "You can't have an empty list item",
+        })
 
-def view_list(request, list_id):
+def view_list(request, list_id, errors=None):
     list_ = List.objects.get(id=list_id)
     return render(request, 'list.html',
         {
         'list': list_,
+        'errors': errors
         }
     )
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return HttpResponseRedirect(f'/lists/{list_id}/')
+    text = request.POST['item_text']
+    text = text.strip()
+    if text:
+        Item.objects.create(text=text, list=list_)
+        return HttpResponseRedirect(f'/lists/{list_id}/')
+    else:
+        return view_list(request, list_id, "You can't have an empty list item")
