d0d2a05810bb6aa0434a50a314a1ad00ce5169f3
songshu189 <songshu.yu@yahoo.com>
2018-01-09 20:59:56 -0500

    Update prod/fabfile.py and templates

--- prod/fabfile.py
+++ prod/fabfile.py
@@ -3,18 +3,17 @@ from fabric.contrib.files import append, exists, upload_template
 from fabric.api import cd, env, local, run, sudo
 from fabric.colors import green, yellow
 
-from django.conf import settings
-conf = settings.FABRIC
+from fab_conf import FABRIC as conf
 
-REPO_URL = 'https://github.com/hjwp/book-example.git'
+
 env.REPO_URL = conf.get("REPO_URL")
 env.user = conf.get("user", "ubuntu")
 env.project = conf.get("project", "myproject")
 env.host_ip = conf.get("host_ip", "127.0.0.1")
 env.hosts = conf.get("HOSTS", [""])
+env.key_filename = conf.get("key_filename")
 env.nginx = f"/etc/nginx/sites-available/{env.project}"
 env.site_folder = f'/home/{env.user}/sites/{env.project}'
-
 
 def update():
     site_folder = env.site_folder
@@ -22,32 +21,36 @@ def update():
        run(f'mkdir -p {site_folder}')
     with cd(site_folder):
         _get_latest_source()
-        _update_pipenv()
-        _update_files()
-        _update_database()
+        changed_files = run('git diff --name-only HEAD~ HEAD').splitlines()
+        print('changed_files', changed_files)
+        if 'Pipfile' in changed_files:
+           _update_pipenv()
+        _update_setup_files()
+        if any([file.endswith('models.py') for file in changed_files]):
+            _update_database()
 
 def _get_latest_source():
     if exists('.git'):
         run('git fetch')
     else:
-        run(f'git clone {REPO_URL} .')
-    current_commit = local("git log -n 1 --format=%H", capture=True)
-    run(f'git reset --hard {current_commit}')
-    print(green(''))
+        run(f'git clone {env.REPO_URL} .')
+    # current_commit = local("git log -n 1 --format=%H", capture=True)
+    # run(f'git reset --hard {current_commit}')
+    print(green('get latese source'))
 
 def _update_pipenv():
     if not run('which pipenv'):
-        run(f'pip instsll --user pipenv')
+        run(f'pip3 install --user pipenv')
     run('pipenv install')
     print(green('package installed'))
 
-def update_files():
+def _update_setup_files():
     nginx_folder = f'/etc/nginx/sites-available/{env.project}'
-    upload_template('deploy/nginx.template',
+    upload_template('prod/nginx.template',
                     nginx_folder,
                     env, use_sudo=True, backup=False
                     )
-    upload_template('deploy/local_settings.py.template',
+    upload_template('prod/local_settings.py.template',
         f'/home/{env.user}/sites/{env.project}/{conf["folder"]}/local_settings.py',
         env, use_sudo=True, backup=False
         )
@@ -58,5 +61,7 @@ def update_files():
     print(green('gunicorn and nginx restarted'))
 
 def _update_database():
+    run('pipenv shell')
     run('python manage.py makemigrations')
     run('python manage.py migrate')
+    run('exit')
